ad706a90a03ae39049e521f7962584595f3e7466
EmiRoberti77 <emi@datamotion.ai>
2024-11-01 11:32:42 +0000

    added image editing with mark feature for titan

--- src/image/Titan/generate_image_titan.py
+++ src/image/Titan/generate_image_titan.py
@@ -7,7 +7,7 @@ client = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
 titan_image_config=json.dumps({
     "taskType":"TEXT_IMAGE",
     "textToImageParams":{
-        "text":"a dog sleeping on a sofa",
+        "text":"a tiger in the forest by a river",
     },
     "imageGenerationConfig":{
         "numberOfImages":1,
@@ -27,7 +27,7 @@ response_body = json.loads(response.get("body").read())
 base64_image = response_body.get("images")[0]
 decodedd_base64Image = base64.b64decode(base64_image)
 
-with open('dog.png', "wb") as f:
+with open('tiger.png', "wb") as f:
     f.write(decodedd_base64Image)
 
 print('image complete')
