172d5ff3fded62534c9e2575243610ad064d4afb
EmiRoberti77 <emi@datamotion.ai>
2024-11-01 07:36:33 +0000

    image Titan model

---
+++ src/image/Titan/titan.py
@@ -0,0 +1,33 @@
+import boto3
+import json
+import base64
+
+client = boto3.client(service_name="bedrock-runtime", region_name="us-east-1")
+
+titan_image_config=json.dumps({
+    "taskType":"TEXT_IMAGE",
+    "textToImageParams":{
+        "text":"a dog sleeping on a sofa",
+    },
+    "imageGenerationConfig":{
+        "numberOfImages":1,
+        "height":512,
+        "width":512,
+        "cfgScale":8.0
+    }
+})
+
+response = client.invoke_model(
+    body=titan_image_config,
+    modelId="amazon.titan-image-generator-v1",
+    accept="application/json",
+    contentType="application/json")
+
+response_body = json.loads(response.get("body").read())
+base64_image = response_body.get("images")[0]
+decodedd_base64Image = base64.b64decode(base64_image)
+
+with open('dog.png', "wb") as f:
+    f.write(decodedd_base64Image)
+
+print('image complete')
